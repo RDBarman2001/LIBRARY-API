606fa64648aca27837e4fcb6c565eeb25cdc7872
RDBarman2001 <rupakdebbarman111@gmail.com>
2022-11-15 18:39:56 +0530

    final

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const express=require("express");
-const dotenv=require("dotenv");
-const Db=require('./databaseConnection')
-const usersRouter=require('./routes/users')
-const booksRouter=require('./routes/books')
+const express = require("express");
+const dotenv = require("dotenv");
+// database connection
+const DbConnection = require("./databaseConnection");
+// importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
 dotenv.config();
-const app =express();
-Db();
-const PORT=8081;
+
+const app = express();
+
+DbConnection();
+
+const PORT = 8081;
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.status(200).json({
-        message:"Server is running",
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is up and running",
+  });
 });
 
-app.use('/users',usersRouter);
-app.use('/books',booksRouter);
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
- app.get('*',(req,res)=>{
-    res.status(404).json({
-        message:"Route Not Found",
-    });
- });
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "This route does not exist",
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is running a port${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
 });
--- routes/books.js
+++ routes/books.js
@@ -1,108 +1,65 @@
-const express=require('express');
-const { getAllBooks, getBookbyid, deleteBookbyid, getAllissuedbook, getAllissuedbooks, addNewBook, updateBookById, getSingleBookByName, getBookByName } = require('../controllers/books.cont');
+const express = require("express");
 const { books } = require("../data/books.json");
 const { users } = require("../data/users.json");
-const router =express.Router();
-/* 
-Route:/books
-Method:GET
-Description: Get all books
-Access: public
-Parameters:None
+const {
+  getAllBooks,
+  getSingleBookById,
+  getAllIssuedBooks,
+  addNewBook,
+  updateBookById,
+  getSingleBookByName,
+} = require("../controllers/book-controller");
+
+const router = express.Router();
+
+/**
+ * Route: /books
+ * Method: GET
+ * Description: Get all books
+ * Access: Public
+ * Parameters: none
  */
-router.get('/',getAllBooks)
-/* 
-Route:/books/:id
-Method:GET
-Description: Get book by id
-Access: public
-Parameters:id
+router.get("/", getAllBooks);
+
+/**
+ * Route: /books/:id
+ * Method: GET
+ * Description: Get book by id
+ * Access: Public
+ * Parameters: id
  */
-router.get('/:id',getBookbyid)
-// router.get('/getbook/name/:name',getBookByName)
+router.get("/:id", getSingleBookById);
 
-/* 
-Route:/books
-Method:POST
-Description:Creat new book
-Access: public
-Parameters: none
+router.get("/getbook/name/:name", getSingleBookByName);
+
+/**
+ * Route: /books/issued/by-user
+ * Method: GET
+ * Description: Get all issued books
+ * Access: Public
+ * Parameters: none
  */
-router.post('/',addNewBook)
+router.get("/issued/by-user", getAllIssuedBooks);
 
-/* 
-Route:/books/:id
-Method:PUT
-Description:Updating Book data
-Access: public
-Parameters: id
+/**
+ * Route: /books
+ * Method: POST
+ * Description: Create new book
+ * Access: Public
+ * Parameters: none
+ * Data: author, name, genre, price, publisher, id
  */
-router.put('/:id',updateBookById)
-/*
-Route:/books/:id
-Method:DELETE
-Description:Delete a Book by id
-Access: public
-Parameters: id
+router.post("/", addNewBook);
+
+/**
+ * Route: /books/:id
+ * Method: PUT
+ * Description: Update book
+ * Access: Public
+ * Parameters: id
+ * Data: author, name, genre, price, publisher, id
  */
-router.delete('/:id',deleteBookbyid)
+router.put("/:id", updateBookById);
 
-/* 
-Route:/books/issued/by-user
-Method:GET
-Description: Get issued books
-Access: public
-Parameters:None
- */
-router.get('/issued/by-user',getAllissuedbooks);
-/* 
-Route:/books/issued/withFine
-Method:GET
-Description: Get issued books with fine
-Access: public
-Parameters:None
- */
-
- router.get('/issued/withFine',(req,res)=>{
-    const userWithIssuedBooks=users.filter((each)=>{
-    if(each.issuedBook) return each;
-});
-const issuedBooks=[];
-userWithIssuedBooks.forEach((each)=>{
-    const book =books.find((book)=>book.id===each.issuedBook)
-    book.issuedBy=each.name;
-    book.issuedDate=each.issuedDate;
-    book.returnDate=each.returnDate;
-    
-    const getDateIndays =(data="")=>{
-        
-        if(data===""){
-            date=new Date();
-        }
-        else{
-            date=new Date(data)
-        }
-        let days=Math.floor(date/(1000*60*60*24));
-        return days;
-       };
-       let returnDate=getDateIndays(each.returnDate);
-       let currentDate=getDateIndays();
-       if(returnDate<currentDate){
-        issuedBooks.push(book);
-       }
-
-});
-if(issuedBooks.length===0)
-    return res.status(404).json({
-        success:false,
-        message:"No Books issued Yet"
-    });
-    return res.status(200).json({
-        success:true,
-        message:issuedBooks,
-    });
-
-     })
-
-
-module.exports=router;
+// default export
+module.exports = router;
